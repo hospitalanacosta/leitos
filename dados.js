22328c85302e4130bca97fb08a0cdedceaa8a77b
eduardostsbr <eduaoe@gmail.com>
2026-04-21 06:50:10 -0300

    Atualização automática - 21/04/2026 06:50

--- dados.js
+++ dados.js
@@ -1,6 +1,6 @@
-// Gerado em 21/04/2026 06:44
+// Gerado em 21/04/2026 06:50
 const DADOS = {
-  "atualizado_em": "21/04/2026 06:44",
+  "atualizado_em": "21/04/2026 06:50",
   "meses_disponiveis": [
     {
       "chave": "2026-01",
