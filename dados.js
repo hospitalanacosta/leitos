3929ce0762e53402a92903a6a8e1bbe32183ea08
nutricionais <nutricionais.sistema@gmail.com>
2026-05-01 06:26:12 -0300

    Atualiza dashboard de leitos

--- dados.js
+++ dados.js
@@ -1,6 +1,6 @@
-// Gerado em 01/05/2026 06:20
+// Gerado em 01/05/2026 06:26
 const DADOS = {
-  "atualizado_em": "01/05/2026 06:20",
+  "atualizado_em": "01/05/2026 06:26",
   "meses_disponiveis": [
     {
       "chave": "2026-01",
